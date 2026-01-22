ef51efc1f0a2c71e06b7c131481d8bd3e33ccb46
michelenatale <104991886+michelenatale@users.noreply.github.com>
2026-01-22 11:18:29 +0100

    Update

---
+++ LanguageInteroperability/CsToC/CsToC.cs
@@ -0,0 +1,11 @@
+﻿
+ 
+
+//Switch to x64 architecture.
+
+namespace LanguageInteroperability;
+
+public partial class CsToC
+{
+
+}
---
+++ LanguageInteroperability/CsToC/PInvoke/cadd.c
@@ -0,0 +1,6 @@
+﻿
+
+__declspec(dllexport) int add(int a, int b)
+{
+  return a + b; 
+}
